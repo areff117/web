646a538b842830f3967f2e0862cf84064824601e
Research <132883334+areff117@users.noreply.github.com>
2025-12-04 05:33:13 -0600

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+
+if (navToggle && nav) {
+  navToggle.addEventListener("click", () => {
+    nav.classList.toggle("is-open");
+  });
+}
